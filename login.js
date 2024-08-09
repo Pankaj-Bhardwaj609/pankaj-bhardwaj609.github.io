7fdc5be0114514f2f518bce3ac30441f2aab9526
Pankaj Bhardwaj <bhardwajpankaj609@gmail.com>
2024-08-09 23:41:50 +0530

    test

--- login.js
+++ login.js
@@ -1,4 +1,24 @@
 $(".message a").click(function () {
    $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
  });
- 
+ document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  // Get the values of the input fields
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+  // Hardcoded credentials for demonstration
+  var correctUsername = "admin";
+  var correctPassword = "123";
+
+  // Check if the entered credentials match the hardcoded ones
+  if (username === correctUsername && password === correctPassword) {
+      document.getElementById('message').innerText="Welcome";
+      // Redirect to a new page if the credentials are correct
+      window.location.href = "home.html";
+  } else {
+      // Display an error message if the credentials are incorrect
+      document.getElementById('message').innerText = "Invalid username or password. Please try again.";
+  }
+});
